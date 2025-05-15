eea997c7207137b61426170be0cf928a5f4a80b8
Eduardo Casati <escasati@gmail.com>
2025-05-15 18:46:23 -0400

    Implementa a lógica do hook useCountdown

---
+++ src/pages/Home/Countdown/useCountdown.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+
+export function useCountdown(targetDate) {
+  const getTimeLeft = () => {
+    const now = new Date();
+    const distance = new Date(targetDate) - now;
+
+    if (distance <= 0) {
+      return {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+      };
+    }
+
+    const seconds = Math.floor((distance / 1000) % 60);
+    const minutes = Math.floor((distance / 1000 / 60) % 60);
+    const hours = Math.floor((distance / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+    return {
+      days,
+      hours,
+      minutes,
+      seconds,
+    };
+  };
+
+  const [timeLeft, setTimeLeft] = useState(getTimeLeft());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeLeft(getTimeLeft());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [targetDate]);
+
+  return timeLeft;
+}
