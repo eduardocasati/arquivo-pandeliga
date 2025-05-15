b1ba5f950701b476af279e46e9b3795f393b4ace
Eduardo Casati <escasati@gmail.com>
2025-05-14 22:16:38 -0400

    Organização inicial de pastas e arquivos

---
+++ src/constants/playersCarousel.js
@@ -0,0 +1,17 @@
+import defense from '../assets/images/players/defense.jpg';
+import kicker from '../assets/images/players/kicker.jpg';
+import quarterback from '../assets/images/players/quarterback.jpg';
+import runningback from '../assets/images/players/runningback.jpg';
+import tightend from '../assets/images/players/tightend.jpg';
+import widereceiver from '../assets/images/players/widereceiver.jpg';
+
+const playersCarousel = [
+  { name: 'Lamar Jackson', image: quarterback },
+  { name: 'Saquon Barkley', image: runningback },
+  { name: 'Ja\'Marr Chase', image: widereceiver },
+  { name: 'Brock Bowers', image: tightend },
+  { name: 'Brandon Aubrey', image: kicker },
+  { name: 'Broncos DST', image: defense }
+]
+
+export default playersCarousel
