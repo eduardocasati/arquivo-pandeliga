fdf7704224024fb981723d356673ca58ab33ec69
Eduardo Casati <escasati@gmail.com>
2025-06-05 01:05:19 -0400

    Refatora mobile menu para usar classes condicionais / adiciona tema cinza / adiciona favicons e título da página

--- src/components/Header/MobileMenu/MobileMenu.jsx
+++ src/components/Header/MobileMenu/MobileMenu.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { CgClose } from 'react-icons/cg';
+import { CgClose, CgMenu } from 'react-icons/cg';
 
 import teamList from '../../../constants/teamList';
 
@@ -15,51 +15,53 @@ export const MobileMenu = () => {
   return (
     <>
       <div
-        className={`header__mobile-menu-icon${
-          isMenuOpen ? ' mobile-menu-icon--active' : ''
-        }`}
+        className="header__mobile-menu-icon"
         onClick={handleMobileMenuToggle}
       >
-        <div className="mobile-menu-icon__line" />
+        <CgMenu />
       </div>
 
-      {/* Renderiza o overlay e o menu apenas quando o menu está aberto */}
-      {isMenuOpen && <div className="header__mobile-menu-overlay" />}
-      {isMenuOpen && (
-        <div className="header__mobile-menu-nav">
-          <div className="mobile-menu-nav__top">
-            <img src={logoArquivoPandeliga} alt="Arquivo Pandeliga Logo" />
-            <div
-              className="mobile-menu-icon__close"
-              onClick={handleMobileMenuToggle}
-            >
-              <CgClose />
-            </div>
-          </div>
+      <div
+        className={`header__mobile-menu-overlay${isMenuOpen ? ' mobile-menu--open' : ''}`}
+        aria-hidden={!isMenuOpen}
+      />
 
-          <ul className="header__mobile-nav-list">
-            <li className="header__mobile-nav-item">Home</li>
-            <li className="header__mobile-nav-item">Confrontos Diretos</li>
-            <li className="header__mobile-nav-item">Recordes</li>
-            <li className="header__mobile-nav-item">Classificação Histórica</li>
-            <li className="header__mobile-nav-item">Temporadas</li>
-            <li className="header__mobile-nav-item">Sala de Troféus</li>
-            <li className="header__mobile-nav-item">Times</li>
-          </ul>
-          <ul className="header__mobile-team-list">
-            {teamList.map((team) => {
-              return (
-                <li
-                  style={{ '--team-color': `var(--team-${team.team_id})` }}
-                  key={team.team_id}
-                >
-                  {team.display_name}
-                </li>
-              );
-            })}
-          </ul>
+      <div
+        className={`header__mobile-menu${isMenuOpen ? ' mobile-menu--open' : ''}`}
+        aria-hidden={!isMenuOpen}
+      >
+        <div className="mobile-menu-nav__top">
+          <img src={logoArquivoPandeliga} alt="Arquivo Pandeliga Logo" />
+          <div
+            className="mobile-menu-icon__close"
+            onClick={handleMobileMenuToggle}
+          >
+            <CgClose />
+          </div>
         </div>
-      )}
+
+        <ul className="header__mobile-nav-list">
+          <li className="header__mobile-nav-item">Home</li>
+          <li className="header__mobile-nav-item">Confrontos Diretos</li>
+          <li className="header__mobile-nav-item">Recordes</li>
+          <li className="header__mobile-nav-item">Classificação Histórica</li>
+          <li className="header__mobile-nav-item">Temporadas</li>
+          <li className="header__mobile-nav-item">Sala de Troféus</li>
+          <li className="header__mobile-nav-item">Times</li>
+        </ul>
+        <ul className="header__mobile-team-list">
+          {teamList.map((team) => {
+            return (
+              <li
+                style={{ '--team-color': `var(--team-${team.team_id})` }}
+                key={team.team_id}
+              >
+                {team.display_name}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
 };
