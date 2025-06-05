1fa1f5068efbaa1af9a8a2a583e4ad1a06fc533c
Eduardo Casati <escasati@gmail.com>
2025-06-05 02:00:35 -0400

    Adiciona efeito sanfona na lista de times do menu mobile

--- src/components/Header/MobileMenu/MobileMenu.jsx
+++ src/components/Header/MobileMenu/MobileMenu.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { CgClose, CgMenu } from 'react-icons/cg';
+import { MdKeyboardArrowDown } from 'react-icons/md';
 
 import teamList from '../../../constants/teamList';
 
@@ -9,13 +10,16 @@ import './MobileMenu.css';
 
 export const MobileMenu = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isTeamlistOpen, setIsTeamlistOpen] = useState(false);
 
   const handleMobileMenuToggle = () => setIsMenuOpen(!isMenuOpen);
+
+  const handleTeamlistToggle = () => setIsTeamlistOpen(!isTeamlistOpen);
 
   return (
     <>
       <div
-        className="header__mobile-menu-icon"
+        className="header__mobile-menu-icon noselect"
         onClick={handleMobileMenuToggle}
       >
         <CgMenu />
@@ -47,20 +51,33 @@ export const MobileMenu = () => {
           <li className="header__mobile-nav-item">Classificação Histórica</li>
           <li className="header__mobile-nav-item">Temporadas</li>
           <li className="header__mobile-nav-item">Sala de Troféus</li>
-          <li className="header__mobile-nav-item">Times</li>
+          <li
+            className="header__mobile-nav-item noselect"
+            onClick={handleTeamlistToggle}
+          >
+            Times
+            <span className={isTeamlistOpen && 'arrow-rotate-up'}>
+              <MdKeyboardArrowDown />
+            </span>
+          </li>
         </ul>
-        <ul className="header__mobile-team-list">
-          {teamList.map((team) => {
-            return (
-              <li
-                style={{ '--team-color': `var(--team-${team.team_id})` }}
-                key={team.team_id}
-              >
-                {team.display_name}
-              </li>
-            );
-          })}
-        </ul>
+        <div
+          className={`header__mobile-team-list${isTeamlistOpen ? ' team-list--open' : ''}`}
+          aria-hidden={!isTeamlistOpen}
+        >
+          <ul>
+            {teamList.map((team) => {
+              return (
+                <li
+                  style={{ '--team-color': `var(--team-${team.team_id})` }}
+                  key={team.team_id}
+                >
+                  {team.display_name}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </>
   );
