0cfaa74fa72232a85072b40400ade6cf043be246
Eduardo Casati <escasati@gmail.com>
2025-06-09 18:04:56 -0400

    Melhora a responsividade do ChampionCard

--- src/pages/Home/ChampionCard/ChampionCard.jsx
+++ src/pages/Home/ChampionCard/ChampionCard.jsx
@@ -14,43 +14,53 @@ export const ChampionCard = () => {
   // TO DO: Adicionar a lógica que pega os stats do campeão de forma dinâmica
   return (
     <div className="champion-card">
+      {/* logo do campeão como imagem de fundo */}
+      <img
+        className="champion-card__background-logo"
+        src={teamInfo.team_logo}
+        alt=""
+        aria-hidden="true"
+      />
+
       <div className="champion-card__champion-info">
         <h1>Atual Campeão</h1>
         <h2>{teamInfo.team_name}</h2>
         <h3>{teamInfo.user_name}</h3>
-        <div className="champion-card__champion-stats">
-          <div className="stats-regular">
-            <h4>TEMP REGULAR</h4>
-            <p>
-              V-D <span>10-4</span>
-            </p>
-            <p>
-              PF <span>1929,90</span>
-            </p>
-            <p>
-              PPJ <span>137,85</span>
-            </p>
-            <p>
-              Roster Moves <span>16</span>
-            </p>
-          </div>
-          <div className="stats-playoffs">
-            <h4>PLAYOFFS</h4>
-            <p>
-              Bye <span>Sim</span>
-            </p>
-            <p>
-              PF <span>316,28</span>
-            </p>
-            <p>
-              PPJ <span>158,14</span>
-            </p>
-          </div>
+      </div>
+
+      <div className="champion-card__champion-stats">
+        <div className="stats-regular">
+          <h4>TEMP REGULAR</h4>
+          <p>
+            V-D <span>10-4</span>
+          </p>
+          <p>
+            PF <span>1929,90</span>
+          </p>
+          <p>
+            PPJ <span>137,85</span>
+          </p>
+          <p>
+            Roster Moves <span>16</span>
+          </p>
+        </div>
+        <div className="stats-playoffs">
+          <h4>PLAYOFFS</h4>
+          <p>
+            Bye <span>Sim</span>
+          </p>
+          <p>
+            PF <span>316,28</span>
+          </p>
+          <p>
+            PPJ <span>158,14</span>
+          </p>
         </div>
       </div>
-      <div className="champion-card__champion-logo">
+
+      {/* <div className="champion-card__champion-logo">
         <img src={teamInfo.team_logo} alt={`${teamInfo.team_name} Logo`} />
-      </div>
+      </div> */}
     </div>
   );
 };
