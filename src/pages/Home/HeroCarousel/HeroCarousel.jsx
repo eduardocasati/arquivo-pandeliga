1790b9722540a79aabffe20bf16e377626f6300d
Eduardo Casati <escasati@gmail.com>
2025-06-11 14:51:11 -0400

    Adiciona lógica para mudar a cor do efeito duotone a cada nova imagem em HeroCarousel

--- src/pages/Home/HeroCarousel/HeroCarousel.jsx
+++ src/pages/Home/HeroCarousel/HeroCarousel.jsx
@@ -8,12 +8,14 @@ import playersCarousel from '../../../constants/playersCarousel';
 
 export const HeroCarousel = () => {
   const [slidesIndex, setSlidesIndex] = useState(1);
+  const [duotoneColor, setDuotoneColor] = useState(1);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       setSlidesIndex((prevIndex) =>
         prevIndex === playersCarousel.length ? 1 : prevIndex + 1,
       );
+      setDuotoneColor((prevIndex) => (prevIndex === 3 ? 1 : prevIndex + 1));
     }, 6000);
     return () => clearInterval(intervalId);
   }, []);
@@ -22,7 +24,12 @@ export const HeroCarousel = () => {
     <div className="hero">
       <figure className="hero__container">
         <div className="effect-duotone__layer">
-          <div className="effect-duotone__layer-inner" />
+          <div
+            className="effect-duotone__layer-inner"
+            style={{
+              '--duotone-color': `var(--color-duotone-${duotoneColor})`,
+            }}
+          />
         </div>
         {playersCarousel.map((slide, index) => (
           <img
