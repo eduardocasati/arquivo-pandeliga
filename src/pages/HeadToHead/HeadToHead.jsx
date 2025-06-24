1ebe89746c6f8c5a0ec28406aff0b11e3a44df9a
Eduardo Casati <escasati@gmail.com>
2025-06-24 00:35:46 -0400

    refactor: adiciona fonte Montserrat no projeto
    feature: inicia html do formulário em HeadtoHead

--- src/pages/HeadToHead/HeadToHead.jsx
+++ src/pages/HeadToHead/HeadToHead.jsx
@@ -6,8 +6,33 @@ export const HeadToHead = () => {
   return (
     <>
       <Header />
-      <div>
-        <h1>PÁGINA DOS CONFRONTOS</h1>
+      <div className="page-wrapper">
+        <form>
+          <label htmlFor="">
+            Time 1:
+            <select name="" id="">
+              <option value="" disabled>
+                Escolha um time
+              </option>
+              <option value="">Rohan Riders</option>
+              <option value="">Beholders</option>
+              <option value="">Spirit Breakers</option>
+              <option value="">Ijuí Faceiros</option>
+            </select>
+          </label>
+          <label htmlFor="">
+            Time 2:
+            <select name="" id="">
+              <option value="" disabled>
+                Escolha um time
+              </option>
+              <option value="">Rohan Riders</option>
+              <option value="">Beholders</option>
+              <option value="">Spirit Breakers</option>
+              <option value="">Ijuí Faceiros</option>
+            </select>
+          </label>
+        </form>
       </div>
     </>
   );
