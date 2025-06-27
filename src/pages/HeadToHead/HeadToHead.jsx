4cdd97dd975f5d425c980da7d9cc866b08649f91
Eduardo Casati <escasati@gmail.com>
2025-06-27 04:02:12 -0400

    feature: cria e estiliza o elemento <form> na page HeadToHead

--- src/pages/HeadToHead/HeadToHead.jsx
+++ src/pages/HeadToHead/HeadToHead.jsx
@@ -2,37 +2,77 @@ import { Header } from '../../components/Header/Header';
 
 import './HeadToHead.css';
 
+import { useState } from 'react';
+import teamList from '../../constants/teamList';
+
 export const HeadToHead = () => {
+  const [teamASelected, setTeamASelected] = useState('');
+  const [teamBSelected, setTeamBSelected] = useState('');
+
+  const handleTeamAChange = (event) => {
+    setTeamASelected(event.target.value);
+  };
+
+  const handleTeamBChange = (event) => {
+    setTeamBSelected(event.target.value);
+  };
+
   return (
     <>
       <Header />
-      <div className="page-wrapper">
-        <form>
-          <label htmlFor="">
-            Time 1:
-            <select name="" id="">
-              <option value="" disabled>
-                Escolha um time
-              </option>
-              <option value="">Rohan Riders</option>
-              <option value="">Beholders</option>
-              <option value="">Spirit Breakers</option>
-              <option value="">Ijuí Faceiros</option>
-            </select>
-          </label>
-          <label htmlFor="">
-            Time 2:
-            <select name="" id="">
-              <option value="" disabled>
-                Escolha um time
-              </option>
-              <option value="">Rohan Riders</option>
-              <option value="">Beholders</option>
-              <option value="">Spirit Breakers</option>
-              <option value="">Ijuí Faceiros</option>
-            </select>
-          </label>
-        </form>
+      <div className="page-content__wrapper">
+        <h1 className="mobile-page__title">Confrontos Diretos</h1>
+        <div className="head-to-head__form">
+          <form>
+            <label htmlFor="teamASelect">
+              <select
+                name="teamA"
+                id="teamASelect"
+                value={teamASelected}
+                onChange={handleTeamAChange}
+              >
+                <option value="" disabled>
+                  Selecione um time
+                </option>
+                {teamList.map((team) => (
+                  <option
+                    value={team.team_name}
+                    key={team.team_id}
+                    disabled={team.team_name === teamBSelected ? true : false}
+                  >
+                    {team.team_name}
+                  </option>
+                ))}
+              </select>
+            </label>
+            {/* <span>vs.</span> */}
+            <label htmlFor="">
+              <select
+                name="teamB"
+                id="teamBSelect"
+                value={teamBSelected}
+                onChange={handleTeamBChange}
+              >
+                <option value="" disabled>
+                  Selecione um time
+                </option>
+                {teamList.map((team) => (
+                  <option
+                    value={team.team_name}
+                    key={team.team_id}
+                    disabled={team.team_name === teamASelected ? true : false}
+                  >
+                    {team.team_name}
+                  </option>
+                ))}
+              </select>
+            </label>
+          </form>
+        </div>
+
+        <div className="head-to-head__table">
+          {/* TODO fazer uma lógica que renderiza os logos dos times quando os dois são selecionados */}
+        </div>
       </div>
     </>
   );
