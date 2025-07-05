22e45efea1a8973ae6d471625366d7dfbe06721e
Eduardo Casati <escasati@gmail.com>
2025-07-05 01:41:45 -0400

    style: organiza importações em useAllSeasonsMatchups

--- src/hooks/useAllSeasonsMatchups.js
+++ src/hooks/useAllSeasonsMatchups.js
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { useEffect, useState } from 'react';
 
 import { getAllSeasonsMatchups } from '../services/matchupsService.js';
+
 import {
   getFromLocalStorage,
   saveToLocalStorage,
