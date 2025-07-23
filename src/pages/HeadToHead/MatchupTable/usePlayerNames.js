b02ba49f792bdaa4825a32c46469ed890656a69e
Eduardo Casati <escasati@gmail.com>
2025-07-23 03:59:16 -0400

    remove comentários

--- src/pages/HeadToHead/MatchupTable/usePlayerNames.js
+++ src/pages/HeadToHead/MatchupTable/usePlayerNames.js
@@ -7,17 +7,16 @@ import { STORAGE_KEYS } from '../../../constants/storageKeys';
 const { ALL_PLAYERS_DATA } = STORAGE_KEYS;
 
 const abbreviateName = (fullName) => {
-  if (typeof fullName !== 'string') return ''; // retorna vazio se não for string
+  if (typeof fullName !== 'string') return '';
 
-  const parts = fullName.trim().split(/\s+/); // remove espaços extras e divide pelo espaço
+  const parts = fullName.trim().split(/\s+/);
 
   if (parts.length === 1) {
-    // se só tiver um nome, retorna o nome completo
     return parts[0];
   }
 
-  const firstInitial = parts[0][0].toUpperCase(); // primeira letra maiúscula do primeiro nome
-  const lastName = parts.slice(1).join(' '); // junta todas as palavras após o primeiro nome
+  const firstInitial = parts[0][0].toUpperCase();
+  const lastName = parts.slice(1).join(' ');
 
   return `${firstInitial}. ${lastName}`;
 };
