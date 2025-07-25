ab6130beb84617bcc1ac41ebcf2e4879957eec5c
Eduardo Casati <escasati@gmail.com>
2025-07-25 03:52:54 -0400

    chore: deleta arquivo index.js em /utils

--- src/utils/index.js
+++
@@ -1,8 +0,0 @@
-export { sumPointsFromMatchups } from './aggregators/sumPoints.js';
-export { formatToBRDecimal } from './formatters/numberFormat.js';
-export {
-  clearLocalChampionTeamData,
-  getLocalChampionTeamData,
-  saveChampionTeamData,
-} from './storage/championStorage.js';
-export { hasByeWeekInPlayoffs } from './validators/hasPlayoffByeWeek.js';
