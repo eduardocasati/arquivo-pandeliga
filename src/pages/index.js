b147f6856d53178e193a749dc9770ac868153e4c
Eduardo Casati <escasati@gmail.com>
2025-07-25 03:52:02 -0400

    chore: organiza exportações e importações de /pages

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export { HeadToHead } from './HeadToHead/HeadToHead.jsx';
+export { HistoricalStandings } from './HistoricalStandings/HistoricalStandings.jsx';
+export { Home } from './Home/Home.jsx';
+export { Records } from './Records/Records.jsx';
+export { Seasons } from './Seasons/Seasons.jsx';
+export { TrophyRoom } from './TrophyRoom/TrophyRoom.jsx';
--- src/routes.ts
+++ src/routes.ts
@@ -4,12 +4,14 @@ import {
   createRouter,
   Outlet,
 } from '@tanstack/react-router';
-import { HeadToHead } from './pages/HeadToHead/HeadToHead';
-import { HistoricalStandings } from './pages/HistoricalStandings/HistoricalStandings';
-import { Home } from './pages/Home/Home';
-import { Records } from './pages/Records/Records';
-import { Seasons } from './pages/Seasons/Seasons';
-import { TrophyRoom } from './pages/TrophyRoom/TrophyRoom';
+import {
+  HeadToHead,
+  HistoricalStandings,
+  Home,
+  Records,
+  Seasons,
+  TrophyRoom,
+} from './pages';
 
 // Rota raiz (usada como wrapper)
 const rootRoute = createRootRoute({
