e970193cf7136ee2f75f95de10d99e1f353cee1f
Eduardo Casati <escasati@gmail.com>
2025-07-25 22:26:46 -0400

    cria script generatePlayersFile.js que cria arquivo json com os jogadores

---
+++ src/data/generatePlayersFile.js
@@ -0,0 +1,14 @@
+import { writeFile } from 'fs/promises';
+import { getSimplifiedPlayers } from './getSimplifiedPlayers.js';
+
+async function generateFile() {
+  try {
+    const players = await getSimplifiedPlayers();
+    await writeFile('simplifiedPlayers.json', JSON.stringify(players, null, 2));
+    console.log('Arquivo "simplifiedPlayers.json" criado com sucesso!');
+  } catch (error) {
+    console.error('Erro ao gerar o arquivo:', error);
+  }
+}
+
+generateFile();
