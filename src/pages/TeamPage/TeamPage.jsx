ae2dba9e9981f9a40b0656a110785792dca059ec
Eduardo Casati <escasati@gmail.com>
2025-08-12 10:39:48 -0400

    add estrutura basica html/css TeamPage

--- src/pages/TeamPage/TeamPage.jsx
+++ src/pages/TeamPage/TeamPage.jsx
@@ -3,15 +3,30 @@ import { Route } from '../../routes/$teamId';
 
 import './TeamPage.css';
 
+import teams from '../../constants/teams';
+
 export const TeamPage = () => {
   const { teamId } = Route.useLoaderData();
+
+  const findTeamImage = (teamId) => {
+    return teams.find((team) => team.team_id === teamId).team_logo;
+  };
+
+  const findTeamName = (teamId) => {
+    return teams.find((team) => team.team_id === teamId).team_name;
+  };
 
   return (
     <>
       <Header />
-      <div>
-        <h1>Time: {teamId}</h1>
-        <h1>funcionando</h1>
+      <div className="team-page">
+        <div className="team-page__title">
+          <img
+            src={findTeamImage(teamId)}
+            alt={`${findTeamName(teamId)} Logo`}
+          />
+          <h1>{findTeamName(teamId)}</h1>
+        </div>
       </div>
     </>
   );
